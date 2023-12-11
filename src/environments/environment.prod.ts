c9fc75b0f85d3d985e60889cf19f3bb9528fa0cc
rejikrish83 <rejikrish83@gmail.com>
2023-12-11 10:56:15 +0000

    Docker build changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:8002/v1',
-    OktaDomain: 'dev-41891240.okta.com',
-    ClientID: '0oadnnucz42aQSm9g5d7',
+    OktaDomain: 'dev-xyz.okta.com',
+    ClientID: '12jkjret',
     
   };
